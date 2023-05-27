794145b5b3aa8ac174c55d6f28326bdd18bd0ba9
DiegoCaceresC <diego.caceres.canova@gmail.com>
2023-05-27 00:19:23 -0500

    Second commit Mongoose setup

--- server/index.js
+++ server/index.js
@@ -6,12 +6,12 @@ import dotenv from "dotenv";
 import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
-import { path } from "path";
+import path from "path";
 import { fileURLToPath } from "url";
 
 /*CONFIGURATIONS*/
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.__dirname(__filename);
+const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -21,15 +21,31 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets",express.static(path.join(__dirname,"public/assets")))
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 // FILE STORAGE
 const storage = multer.diskStorage({
-    destination: function (req,file,cb){
-        cb(null,"public/assets");
-    },
-    filename: function (req,file,cb){
-        cb(null,file.originalname);
-    }
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
-const upload = multer({storage})
+const upload = multer({ storage });
+
+// MONGOOSE SETUP
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server raised in port: http://localhost:${PORT}`));
+    app.get("/",(req,res)=>{
+        res.status(200).json({message: "in first server with MONGODB"})
+    });
+  })
+  .catch((error) => console.log(`${error} did not connect`));
+
